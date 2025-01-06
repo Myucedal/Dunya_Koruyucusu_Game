d0993a700297815d1b952368de2aa0a0164e2e93
Emirhan Işık <124465712+EmirTheHan@users.noreply.github.com>
2025-01-06 07:59:52 +0300

    1

--- Assets/1 Cop Toplama Sahnesi/Scripts/Jetpack_Input.cs
+++ Assets/1 Cop Toplama Sahnesi/Scripts/Jetpack_Input.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Jetpack_Input : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float upSpeed = 2000;
+    public float upSpeed = 2000f;
     private AudioSource audioSource;
+    public ParticleSystem jetpackEffect;
+    private bool isPlaying = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,48 +18,66 @@ public class Jetpack_Input : MonoBehaviour
 
     void Update()
     {
-        
 #if UNITY_EDITOR
         if (Input.GetMouseButton(0))
         {
-            if (rb != null)
-            {
-                rb.AddForce(new Vector2(0, upSpeed * Time.deltaTime));
-
-            }
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ActivateJetpack();
         }
         else
         {
-            if (audioSource.isPlaying)
-            {
-                audioSource.Stop();
-            }
+            DeactivateJetpack();
         }
 #endif
+
         if (Input.touchCount > 0)
         {
             Touch dokunma = Input.GetTouch(0);
             if (dokunma.phase == TouchPhase.Stationary || dokunma.phase == TouchPhase.Moved)
             {
-                rb.AddForce(new Vector2(0, upSpeed * Time.deltaTime));
-
-            }
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
+                ActivateJetpack();
             }
         }
         else
         {
+            DeactivateJetpack();
+        }
+    }
+
+    void ActivateJetpack()
+    {
+        if (rb != null)
+        {
+            rb.AddForce(new Vector2(0, upSpeed * Time.deltaTime));
+        }
+
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+            if (!jetpackEffect.isPlaying)
+            {
+                jetpackEffect.Play();
+            }
+        }
+    }
+
+    void DeactivateJetpack()
+    {
+        if (isPlaying)
+        {
+            isPlaying = false;
             if (audioSource.isPlaying)
             {
                 audioSource.Stop();
+            }
+
+            if (jetpackEffect.isPlaying)
+            {
+                jetpackEffect.Stop();
             }
         }
     }
